55ac6324c3bca228f2ea68b34175d7ac0bedde5d
AgiliaErnis <ernis.agilia@gmail.com>
2021-02-07 21:36:47 +0100

    loops

---
+++ loop.py
@@ -0,0 +1,12 @@
+for i in [1,2,3,4,5]:
+    print(i)
+
+print("Different loop...")
+
+for ch in "Hello there!":
+    print(ch)
+
+print("Different loop...")
+
+for i in range(7):
+    print("Head First Rocks!")
